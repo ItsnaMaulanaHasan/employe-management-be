e06cf6ee7fdcc0e96b47ee87e761bd000a158b18
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-01-07 01:26:00 +0700

    feat: create route for employee

---
+++ internal/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"be-employee-management/internal/database"
+	"be-employee-management/internal/employee"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRoutes(r *gin.Engine) {
+	employeeRepo := employee.NewRepository(database.DB)
+	employeeService := employee.NewService(employeeRepo)
+	employeeHandler := employee.NewHandler(employeeService)
+
+	apiV1 := r.Group("/api/v1")
+	apiV1.GET("/employees/active/smith", employeeHandler.GetActiveSmithEmployees)
+}
