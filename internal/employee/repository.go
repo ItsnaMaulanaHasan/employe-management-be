436a2565c2bf985e0a57d6a26041c945353ad1ba
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-01-07 01:42:19 +0700

    feat: add method for find employee without reviews

--- internal/employee/repository.go
+++ internal/employee/repository.go
@@ -8,6 +8,7 @@ import (
 
 type Repository interface {
 	FindActiveSmithEmployees(ctx context.Context) ([]EmployeeName, error)
+	FindEmployeesWithoutReviews(ctx context.Context) ([]EmployeeName, error)
 }
 
 type repository struct {
@@ -28,7 +29,30 @@ func (r *repository) FindActiveSmithEmployees(ctx context.Context) ([]EmployeeNa
 	defer rows.Close()
 
 	var employees []EmployeeName
+	for rows.Next() {
+		var e EmployeeName
+		err := rows.Scan(&e.FirstName, &e.LastName)
+		if err != nil {
+			return nil, err
+		}
+		employees = append(employees, e)
+	}
 
+	return employees, nil
+}
+
+func (r *repository) FindEmployeesWithoutReviews(ctx context.Context) ([]EmployeeName, error) {
+	query := `SELECT e.first_name, e.last_name FROM employees e WHERE NOT EXIST (
+				SELECT 1 FROM annual_reviews ar WHERE ar.employee_id = e.id 
+			) ORDER BY e.hire_date`
+
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var employees []EmployeeName
 	for rows.Next() {
 		var e EmployeeName
 		err := rows.Scan(&e.FirstName, &e.LastName)
